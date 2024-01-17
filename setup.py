b7a050e3cbd7a51fcd74c1bdf844c0bd2ac52970
Tobia Ippolito <tobia2761@gmail.com>
2024-01-17 23:58:28 +0100

    Fixed license in setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MPL-2.0',   # Again, pick a license
+    'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',   # Again, pick a license (https://autopilot-docs.readthedocs.io/en/latest/license_list.html)
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',      # Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.10',
